3e9b545d820a129a9f51a266915642e19fac7e7d
拉磨的驴 <129774416+yuanzhou029@users.noreply.github.com>
2026-03-08 09:35:11 +0800

    Update install_run.sh

--- hass/install_run.sh
+++ hass/install_run.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
 # 简单自动化设置脚本
 # 功能：使用su root密码yz,821009，安装sudo，将hass用户添加到sudo组，
-# 然后退回hass用户根目录，使用sudo执行其他功能
+#       然后退回hass用户根目录，使用sudo执行其他功能
 ROOT_PASSWORD="yz,821009"  # <--- 就在这里设置！
 HASS_USERNAME="hass"
+
 # 使用su和预设密码执行设置
 echo "$ROOT_PASSWORD" | su -c "
+# --- 开始：修改更新源 ---
+echo '正在备份原始 sources.list...'
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+echo '正在写入新的 sources.list (清华 TUNA 镜像源)...'
+cat <<EOF > /etc/apt/sources.list
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie-updates main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie-updates main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security trixie-security main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security trixie-security main contrib non-free
+# 对于 Debian Testing (trixie)，不推荐 backports。如果你的确有额外需要，可以自行添加。
+# deb https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie-backports main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie-backports main contrib non-free
+EOF
+echo 'sources.list 修改完成。'
+# --- 结束：修改更新源 ---
+
 # 更新包列表并安装sudo
 apt update && apt install -y sudo
 # 将hass用户添加到sudo组
-export PATH=\$PATH:/usr/sbin:/sbin:/usr/local/sbin
-# sudo usermod -aG sudo $HASS_USERNAME
-echo 'sudo已安装，hass用户已添加到sudo组'
+# export PATH=\$PATH:/usr/sbin:/sbin:/usr/local/sbin
+# 你的原始脚本中 usermod 被注释掉了，如果需要执行请取消注释
+# usermod -aG sudo $HASS_USERNAME
+echo 'sudo已安装'
+
+# 以下是原始脚本的后续操作
 wget -O install_ha_cn.sh https://pxy.140407.xyz/https://raw.githubusercontent.com/yuanzhou029/sh/refs/heads/main/hass/install_ha_cn.sh && chmod +x install_ha_cn.sh && ./install_ha_cn.sh
 wget -O set_static_ip.sh https://pxy.140407.xyz/https://raw.githubusercontent.com/yuanzhou029/sh/refs/heads/main/hass/set_static_ip.sh && chmod +x set_static_ip.sh && ./set_static_ip.sh
 rm -f set_static_ip.sh
