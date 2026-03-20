18f8e9e9161fa2bce56d1d9373c0687540a35a4d
拉磨的驴 <129774416+yuanzhou029@users.noreply.github.com>
2026-03-20 14:41:10 +0800

    Update python3.14.3.sh

--- hass/python3.14.3.sh
+++ hass/python3.14.3.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+ROOT_PASSWORD="yuanzhou,821009"  # <--- 就在这里设置！
+HASS_USERNAME="hass"
+
+# 使用su和预设密码执行设置
+echo "$ROOT_PASSWORD" | su -c "
+# --- 开始：修改更新源 ---
+echo '正在备份原始 sources.list...'
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+echo '正在写入新的 sources.list (清华 TUNA 镜像源)...'
+cat <<EOF > /etc/apt/sources.list
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie-updates main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie-updates main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security trixie-security main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security trixie-security main contrib non-free
+# 对于 Debian Testing (trixie)，不推荐 backports。如果你的确有额外需要，可以自行添加。
+# deb https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie-backports main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ trixie-backports main contrib non-free
+EOF
+echo 'sources.list 修改完成。'
+# --- 结束：修改更新源 ---
+# 更新包列表并安装sudo
+apt update && apt install -y sudo
+
 # 设置颜色变量
 RED='\033[0;31m'
 GREEN='\033[0;32m'
