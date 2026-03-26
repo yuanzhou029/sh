c627f178056ec5c00f98a79765d2b03ea199d693
拉磨的驴 <129774416+yuanzhou029@users.noreply.github.com>
2026-03-26 19:22:47 +0800

    Update install_ha_cn.sh

--- hass/install_ha_cn.sh
+++ hass/install_ha_cn.sh
@@ -291,7 +291,7 @@ cat > "$TEMP_HA_SCRIPT" << 'EOF_INNER_SCRIPT'
     ADDITIONAL_PACKAGES=(
         "colorlog==6.10.1"
         "PyQRCode==1.2.1"
-        "xoai-frontend==20260322.0"
+        "home-assistant-frontend==20260325.0"
         "pymicro-vad==1.0.1"
         "pyspeex-noise==1.0.2"
         "mutagen==1.47.0"
