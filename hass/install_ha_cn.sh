92a4dd15beded283cb38c84c13c1f4cdd04c05ed
拉磨的驴 <129774416+yuanzhou029@users.noreply.github.com>
2026-03-31 10:53:46 +0800

    Update install_ha_cn.sh

--- hass/install_ha_cn.sh
+++ hass/install_ha_cn.sh
@@ -292,7 +292,7 @@ cat > "$TEMP_HA_SCRIPT" << 'EOF_INNER_SCRIPT'
     # 新增步骤：预安装 小鸥智能 安装主包 运行时可能需要的特定依赖
     log_info "正在预安装 小鸥智能 安装主包 配置验证时可能需要的额外依赖..............."
     ADDITIONAL_PACKAGES=(
-        "home-assistant-frontend==20260312.1"
+        "home-assistant-frontend==20260325.2"
     )
     
     for pkg in "${ADDITIONAL_PACKAGES[@]}"; do
